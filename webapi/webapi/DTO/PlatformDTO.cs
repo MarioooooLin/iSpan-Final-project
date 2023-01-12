b5a986d35f42a11e766cc78f2c873825e8fbbf3a
a0917247 <a0917247@gmail.com>
2023-01-12 11:58:35 +0800

    新增ignore

---
+++ webapi/webapi/DTO/PlatformDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace webapi.DTO
+{
+    public class PlatformsDTO
+    {
+        public int? ArticleId { get; set; }
+        public string? ArticleName { get; set; }
+        public int? Authorld { get; set; }
+        public string? Contents { get; set; }
+        public DateTime? UpdateTime { get; set; }
+    }
+}
