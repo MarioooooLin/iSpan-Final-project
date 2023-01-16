93a940ab3709b43326bdfcca37334ab7f47b6304
ChiHsuan <tomlo86052100@gmail.com>
2023-01-16 17:54:07 +0800

    0116

--- webapi/webapi/DTO/PlatformsDTO.cs
+++ webapi/webapi/DTO/PlatformsDTO.cs
@@ -2,9 +2,9 @@
 {
     public class PlatformsDTO
     {
-        public int? ArticleId { get; set; }
+        public int ArticleId { get; set; }
         public string? ArticleName { get; set; }
-        public int? Authorld { get; set; }
+        public int Authorld { get; set; }
         public string? Contents { get; set; }
         public DateTime? UpdateTime { get; set; }
     }
