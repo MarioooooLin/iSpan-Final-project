3dff33707a4dd5b3353052a3f6e96ef20c67b3d9
jing <a0917247@gmail.com>
2023-01-16 23:03:41 +0800

    新增ignore

---
+++ webapi/webapi/DTO/EnterpriseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace webapi.Controllers
+{
+    public class EnterpriseDTO
+    {
+        public int EnterpriseId { get; set; } 
+        public string? Principal { get; set; }
+        public string? Account { get; set; }
+        public string? Password { get; set; }
+        public int VacancyId { get; internal set; }
+    }
+}
