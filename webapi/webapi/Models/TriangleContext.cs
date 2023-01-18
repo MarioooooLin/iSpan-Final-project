40fc14640b9bbd3c62d1dddc50cf10816b471a16
a0917247 <a0917247@gmail.com>
2023-01-18 14:54:09 +0800

    換新雲端資料庫

--- webapi/webapi/Models/TriangleContext.cs
+++ webapi/webapi/Models/TriangleContext.cs
@@ -141,7 +141,7 @@ namespace webapi.Models
             modelBuilder.Entity<CourseDetail>(entity =>
             {
                 entity.HasKey(e => new { e.CourseId, e.SkillId })
-                    .HasName("PK__CourseDe__A4D778BF12CFB334");
+                    .HasName("PK__CourseDe__A4D778BF68D4D8C0");
             });
 
             modelBuilder.Entity<CourseOrder>(entity =>
@@ -233,8 +233,6 @@ namespace webapi.Models
 
                 entity.ToTable("InterestedCV");
 
-                entity.HasIndex(e => new { e.CandidateId, e.Cvid }, "IX_InterestedCV");
-
                 entity.Property(e => e.Cvid).HasColumnName("CVId");
 
                 entity.Property(e => e.Status).HasColumnName("status");
@@ -245,16 +243,12 @@ namespace webapi.Models
                 entity.HasKey(e => new { e.CandidateId, e.EnterpriseId })
                     .HasName("PK_InterestedEnterprise_1");
 
-                entity.HasIndex(e => new { e.CandidateId, e.EnterpriseId }, "IX_InterestedEnterprise");
-
                 entity.Property(e => e.Status).HasColumnName("status");
             });
 
             modelBuilder.Entity<InterestedPlatformArticle>(entity =>
             {
                 entity.HasKey(e => new { e.CandidateId, e.PlatformArticleId });
-
-                entity.HasIndex(e => new { e.CandidateId, e.PlatformArticleId }, "IX_InterestedPlatformArticle");
 
                 entity.Property(e => e.Status).HasColumnName("status");
             });
@@ -276,13 +270,17 @@ namespace webapi.Models
             {
                 entity.HasKey(e => e.ArticleId);
 
-                entity.Property(e => e.ArticleName).HasMaxLength(10);
+                entity.Property(e => e.ArticleName).HasMaxLength(25);
 
-                entity.Property(e => e.UpdateTime).HasColumnType("date");
+                entity.Property(e => e.UpdateTime)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
             });
 
             modelBuilder.Entity<Reply>(entity =>
             {
+                entity.Property(e => e.ReplyId).ValueGeneratedNever();
+
                 entity.Property(e => e.ArticleName).HasMaxLength(10);
 
                 entity.Property(e => e.ReplyTime).HasColumnType("date");
@@ -341,7 +339,7 @@ namespace webapi.Models
             modelBuilder.Entity<VacancySkill>(entity =>
             {
                 entity.HasKey(e => new { e.VacancyId, e.NeedSkillId })
-                    .HasName("PK__VacancyS__81F22C8AF46E7077");
+                    .HasName("PK__VacancyS__81F22C8A1A5BBD82");
             });
 
             OnModelCreatingPartial(modelBuilder);
