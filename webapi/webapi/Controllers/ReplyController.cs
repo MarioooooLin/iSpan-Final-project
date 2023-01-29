cbc41e067cc7e17540cc90a40aeb0d65ac2a74b0
LoChiHsuan <116322783+LoChiHsuan@users.noreply.github.com>
2023-01-29 15:36:59 +0800

    論壇時間更新

--- webapi/webapi/Controllers/ReplyController.cs
+++ webapi/webapi/Controllers/ReplyController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using webapi.Models;
 
 namespace webapi.Controllers
 {
+    [EnableCors("AllowAny")]
     [Route("api/[controller]")]
     [ApiController]
     public class ReplyController : ControllerBase
@@ -29,16 +31,9 @@ namespace webapi.Controllers
 
         // GET: api/Reply/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Reply>> GetReply(int id)
+        public async Task<ActionResult<IEnumerable<Reply>>> GetReply(int id)
         {
-            var reply = await _context.Reply.FindAsync(id);
-
-            if (reply == null)
-            {
-                return NotFound();
-            }
-
-            return reply;
+            return await _context.Reply.Where(reply => reply.ArticleId == id ).ToArrayAsync();
         }
 
         // PUT: api/Reply/5
